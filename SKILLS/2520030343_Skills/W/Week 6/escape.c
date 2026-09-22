cf2478e990acdfb37336439f012d9aa344793d17
Burramukku Aryan <aryanburramukku2007@gmail.com>
2026-09-22 08:36:18 +0530

    Add files via upload

---
+++ SKILLS/2520030343_Skills/W/Week 6/escape.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    printf("Hello\tWorld\n");
+    printf("This is a \"quoted\" word\n");
+    printf("Backslash: \\\n");
+    printf("Single quote: \'\n");
+
+    return 0;
+}
